ed804866200d4f860bdc1db2025eddab282e2f1d
liangwenwei <liangww@tsintergy.com>
2024-04-09 17:55:39 +0800

    真实考场模拟图片不展示bugfix

---
+++ components/Colloct/index.js
@@ -0,0 +1,43 @@
+// components/colloct/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    showColloct: false,
+  },
+
+  ready() {
+    setTimeout(() => {
+      this.checkColloct()
+    }, 1000 * 60 * 5);
+    // 5分钟后，出现
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onColloct() {
+      wx.setStorageSync('colloct_tag', true)
+      this.setData({
+        showColloct: false
+      })
+    },
+    checkColloct() {
+      const isCheck = wx.getStorageSync('colloct_tag')
+      if (!isCheck) {
+        this.setData({
+          showColloct: true
+        })
+      }
+    }
+  }
+})
